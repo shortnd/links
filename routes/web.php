4f6ae022fbd4e9f618e1b86e461a3e13b3d3e656
shortnd <ococncol@gmail.com>
2018-07-21 09:01:27 -0400

    fixed submit view and now validates and redirects after sumit

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,18 @@ Route::get('/submit', function () {
     return view('submit');
 });
 
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255'
+    ]);
+
+    $link = tab(new App\Link($data))->save();
+
+    return redirect('/');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
